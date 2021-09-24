8c981d71f036726c22731a893989bdd1f4e86a7d
gammazero <gammazero@users.noreply.github.com>
2021-09-24 08:25:30 -0700

    revert incorrect change

--- dspinner/pin.go
+++ dspinner/pin.go
@@ -822,7 +822,7 @@ func (p *pinner) Flush(ctx context.Context) error {
 		return err
 	}
 
-	return p.flushPins(ctx, false)
+	return p.flushPins(ctx, true)
 }
 
 // PinWithMode allows the user to have fine grained control over pin
